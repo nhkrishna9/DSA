6db7562291ef66ff076bd2ee18ca09cf9dda702b
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-05-11 13:10:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n == 0){ return false;}
+        if(n==1){return true;}
+        return isPowerOfThree(n/3);
+        
+    }
+};
