269d3ba0288e7085c954775257897097f363fa47
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-05-12 16:28:00 +0530

    Time: 0 ms (100%), Space: 20.8 MB (27.8%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
-        for(const auto &ele:nums){
-            ans = ans ^ ele;
+        for(const auto & val : nums){
+            ans = ans ^ val;
         }
         return ans;
+        
     }
 };
