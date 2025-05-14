289b6ab32d2df23fe9b3ad159db93172d03d0feb
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-05-14 10:33:03 +0530

    Time: 0 ms (100%), Space: 7.9 MB (32.28%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -4,11 +4,10 @@ public:
 
         while(b!=0){
             int partialSum = a^b; //This will the solution if there is no carry.
-            int carrayBits = a&b;
+            int carrayBits = a&b; // This will identify which bits will have carry. We will loop through carraybit s variable untill it has no carry bits. That mean carrybits=0.
 
-        // Now, Start adding carry to the initial sum 2nd bit and update the carry and use the third bit carry and add it to third bit sum and so on.
+        // Now, Start adding carry to the initial sum 2nd bit and update the carry and use the third bit carry and add it to third bit sum and so on. For this we need to shift the carry bits by 1
             int shiftedCarry = carrayBits<<1;
-
 
             a = partialSum;
             b = shiftedCarry;
