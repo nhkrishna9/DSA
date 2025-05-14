07c17d2ed139f25f5c8ac2d7d80e4713603ce479
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-05-14 16:05:04 +0530

    Time: 0 ms (100%), Space: 13.5 MB (37.56%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        /*
         int ones = 0;
         int twos = 0;
         for(auto ele: nums)
@@ -8,6 +9,23 @@ public:
             ones = (ones^ele) & (~twos);
             twos = (twos^ele) & (~ones);
         }
-        return ones;
+        return ones; */
+
+        //SORTING
+        int n=nums.size();
+        if(n<3)
+            return nums[0];
+        
+        sort(nums.begin(),nums.end());
+        if(nums[0]!=nums[1])
+            return nums[0];
+        int i=1;
+        while(i<n)
+        {
+            if(nums[i]!=nums[i-1])
+                return nums[i-1];
+            i+=3;
+        }
+        return nums[n-1];
     }
 };
