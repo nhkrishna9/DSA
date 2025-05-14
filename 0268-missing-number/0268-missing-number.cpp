0a40787bd68fbdf49168fe5213888054b39f6c23
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-05-14 10:58:24 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.37%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,16 +2,38 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
+
+    
         int ans=0;
         for(int i=0;i<=n;i++){
             ans = ans ^ i;
         }
-
+/*
         for(const auto& num: nums){
             ans = ans ^ num;
         }
 
         return ans;
         
+        */
+
+
+    //std::reduce	Faster parallel-friendly alternative to accumulate (C++17)
+    //std::bit_xor<>	Functional object for XOR operation
+    //std::views::iota(0, n+1)	Lazy range generation from 0 to n (C++20 ranges)
+    //No loops	Entire solution is declarative and clean
+
+        // Compute XOR of all indices [0..n]. Leet code doesn't support C++20 constructs.
+        // int expected = std::reduce(std::views::iota(0, n + 1), 0, std::bit_xor<>());
+
+        // Compute XOR of all elements in nums
+        //int actual = std::reduce(nums.begin(), nums.end(), 0, std::bit_xor<>());
+
+
+        int actual = std::accumulate(nums.begin(), nums.end(), 0, std::bit_xor<>());
+
+
+        return ans ^ actual;
+
     }
 };
