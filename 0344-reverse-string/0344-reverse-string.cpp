2934ad326a5670d7439ec95c0ee28944a6e3e9e3
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-05-15 12:15:04 +0530

    Time: 0 ms (100%), Space: 27.3 MB (52.1%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
+    void reverseHelper(vector<char>& s, int low, int high){
+        if(low > high) return;
+        swap(s[low], s[high]);
+        reverseHelper(s, low+1, high-1);
+    }
     void reverseString(vector<char>& s) {
         int n=s.size();
         int low=0, high=n-1;
+        reverseHelper(s, low, high);
+        /*
         while(low<=high){
             s[high] = s[low]+s[high] - (s[low]=s[high]);
             low++;high--;
         }
+        */
     }
 };
