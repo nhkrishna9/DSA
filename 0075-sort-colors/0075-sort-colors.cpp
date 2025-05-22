906b1f109c2b5945909608f317bbfb4613ef6829
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-05-22 18:14:15 +0530

    Time: 0 ms (100%), Space: 11.7 MB (12.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
+        // Below is bubble sort solution which takes O(N^2)
+        // But, we need to solve it in O(N).
+        /*
         for(int i=0;i<n;i++){
             for(int j=0;j<n-1-i;j++){
                 if(nums[j]>nums[j+1]){
@@ -9,6 +12,23 @@ public:
                 }
             }
         }
-        
+        */
+
+        //O(N) solution
+        int low=0, mid = 0, high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid], nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
     }
 };
