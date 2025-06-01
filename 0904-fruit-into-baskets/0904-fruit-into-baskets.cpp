acbb3b8ccf904562b3bf7b71d447009332dbfecc
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-06-01 13:05:48 +0530

    Time: 41 ms (55.29%), Space: 74.9 MB (76.1%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -9,7 +9,7 @@ public:
         return true;
         
     }
-    int totalFruit(vector<int>& fruits) {
+    int totalFruit_o_n3(vector<int>& fruits) {
         int n = fruits.size();
         int ans = 0;
         for(int i=0;i<n;i++){
@@ -22,5 +22,33 @@ public:
         }
         return ans;
         
+    }
+
+    bool hasAtMostTwoFruitTypes(const unordered_map<int, int>& basket) {
+        return basket.size() <= 2;
+    }
+
+    void removeLeftFruitIfNeeded(unordered_map<int, int>& basket, vector<int>& fruits, int& left) {
+        while (!hasAtMostTwoFruitTypes(basket)) {
+            basket[fruits[left]]--;
+            if (basket[fruits[left]] == 0) {
+                basket.erase(fruits[left]);
+            }
+            left++;
+        }
+    }
+
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int, int> basket; // fruit -> count
+        int left = 0, maxFruits = 0;
+
+        for (int right = 0; right < fruits.size(); right++) {
+            basket[fruits[right]]++; // pick fruit at right position
+            removeLeftFruitIfNeeded(basket, fruits, left); // adjust window
+            int currentFruits = right - left + 1;
+            maxFruits = max(maxFruits, currentFruits);
+        }
+
+        return maxFruits;
     }
 };
