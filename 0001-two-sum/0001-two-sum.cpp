79287cc74b8c5809c0f8484517053abe95874084
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-06-08 07:32:20 +0530

    Time: 0 ms (100%), Space: 14.8 MB (37.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
+//        vector<int> ans;
+        unordered_map<int, int> hash;
         for(int i=0;i<nums.size();i++){
+            if(hash.find(target-nums[i]) != hash.end()){
+                return {hash[target-nums[i]], i};
+            }
+            else{
+                hash[nums[i]] = i;
+            }
+            /*
             for(int j=i+1; j<nums.size();j++){
                 if(nums[i]+nums[j] == target){
                     ans.push_back(i);
                     ans.push_back(j);
                 }
             }
+            */
         }
-        return ans;
+        return {};
     }
 };
