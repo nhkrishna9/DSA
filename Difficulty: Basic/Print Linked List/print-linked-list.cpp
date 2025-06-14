b55d2590235c086050a7448c12292237e0cc1ed7
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-06-14 11:37:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Linked List/print-linked-list.cpp
@@ -0,0 +1,28 @@
+/*
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+/*
+    Print elements of a linked list on console
+    Head pointer input could be NULL as well for empty list
+*/
+
+class Solution {
+  public:
+    // Function to display the elements of a linked list in same line
+    void printList(Node *head) {
+        // your code goes here
+        Node* tmp = head;
+        while(tmp!=nullptr){
+            cout<<tmp->data<<" ";
+            tmp = tmp->next;
+        }
+    }
+};
