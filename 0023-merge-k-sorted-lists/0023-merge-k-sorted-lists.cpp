3ec279d69fd6b227ca80743ee5f4f7bc51897998
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-06-19 20:24:10 +0530

    Time: 5 ms (38.51%), Space: 18.5 MB (51.63%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,56 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* merge2SL(ListNode* l1, ListNode* l2){
-        ListNode* dummy = new ListNode{};
-        ListNode* tmp = dummy;
-        while(l1!=nullptr && l2!= nullptr){
-            if(l1->val <= l2->val){
-                tmp->next = l1;
-                l1 = l1->next;
-                tmp = tmp->next;
-            }
-            else{
-                tmp->next = l2;
-                tmp = tmp->next;
-                l2 = l2->next;
-            }
+    struct compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val; // Min-heap
         }
-        if(l1 != nullptr){
-            tmp->next = l1;
-        }
-        if(l2 != nullptr){
-            tmp->next = l2;
-        }
+    };
 
-        return dummy->next;
-    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0){
-            return nullptr;
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+
+        // Push the head of each list into the heap
+        for (auto l : lists) {
+            if (l != nullptr) {
+                pq.push(l);
+            }
         }
 
-        if(lists.size()==1){
-            return lists[0];
+        ListNode dummy;
+        ListNode* tail = &dummy;
+
+        while (!pq.empty()) {
+            ListNode* curr = pq.top();
+            pq.pop();
+            tail->next = curr;
+            tail = tail->next;
+            if (curr->next != nullptr) {
+                pq.push(curr->next);
+            }
         }
 
-        ListNode* merged = merge2SL(lists[0], lists[1]);
-
-        for(int i=2; i<lists.size(); i++){
-            merged = merge2SL(merged, lists[i]);
-        }
-
-        return merged;
-
-        
+        return dummy.next;
     }
 };
