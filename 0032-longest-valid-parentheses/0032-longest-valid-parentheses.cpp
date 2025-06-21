36f3a40645d94f6d4619b9036fd43e6f3de9988b
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-06-21 12:20:07 +0530

    Time: 2 ms (33.45%), Space: 9.6 MB (94.99%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -3,29 +3,35 @@ public:
     int longestValidParentheses(string s) {
         int open = 0, close = 0, ans = 0;
 
-        // Left to right
+        // Left to right traversal
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '(') open++;
             else close++;
 
             if (open == close) {
-                ans = max(ans, 2 * close);
+                ans = max(ans, open + close);
             } else if (close > open) {
-                open = close = 0;
+                open = 0;
+                close = 0;
             }
         }
 
         open = close = 0;
 
-        // Right to left
+        // Right to left traversal
         for (int i = s.size() - 1; i >= 0; i--) {
-            if (s[i] == '(') open++;
-            else close++;
+            if (s[i] == '('){
+                 open++;
+            }
+            else{
+                close++;
+            }
 
             if (open == close) {
-                ans = max(ans, 2 * open);
+                ans = max(ans, open + close);
             } else if (open > close) {
-                open = close = 0;
+                open = 0;
+                close = 0;
             }
         }
 
