0e361285c7250d46b2c18c0acb797476886ee4bb
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-06-26 06:10:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
         int n = nums.size();
+
+        /*
         vector<long> prefSum(n);
         prefSum[0] = nums[0];
         for(int i=1; i<n; i++){
@@ -14,6 +16,24 @@ public:
             long lsum = prefSum[i];
             long rsum = prefSum[n-1] - prefSum[i];
             if(lsum >= rsum){
+                ++ans;
+            }
+        }
+
+        return ans;
+        */
+
+        int total = 0;
+        for(int i=0; i<n; i++){
+            total += nums[i];
+        }
+
+        int lsum = 0;
+        int ans=0;
+        for(int i=0; i<n-1; i++){
+            lsum = lsum + nums[i];
+            int rsum = total - lsum;
+            if(lsum>=rsum){
                 ++ans;
             }
         }
