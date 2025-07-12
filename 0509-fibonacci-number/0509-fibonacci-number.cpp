25b0d0612c11f959960c303363272aaf6b0f81d9
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2025-07-12 22:16:58 +0530

    Time: 3 ms (66.8%), Space: 8.2 MB (17.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0 || n==1){
-            return n;
+        if(n==0){
+            return 0;
         }
-        return fib(n-1) + fib(n-2);
-        
+        if(n==1){
+            return 1;
+        }
+        int* tmp = new int[n+1];
+        tmp[0] = 0;
+        tmp[1] = 1;
+        for(int i=2; i<=n; i++){
+            tmp[i] = tmp[i-1] + tmp[i-2];
+        }
+        return tmp[n];
+
     }
 };
